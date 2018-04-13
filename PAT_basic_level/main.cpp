fd11c25edd5379eae313ef8f689a8cec6f02dbb0
karlinglee93 <459607025@qq.com>
2018-04-13 23:40:41 +0800

    Initial Commit

---
+++ PAT_basic_level/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAT_basic_level
+//
+//  Created by 李嘉宁 on 2018/4/13.
+//  Copyright © 2018年 李嘉宁. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
