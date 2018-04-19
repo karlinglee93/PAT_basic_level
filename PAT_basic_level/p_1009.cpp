d98634c7f9e1ecc1f9bcc80ac524ae65c1e9ad3f
karlinglee93 <459607025@qq.com>
2018-04-19 23:53:39 +0800

    Update p_1009.cpp; <string> stoi() 将字符串转为int, to_string() 将数值转为字符串; <algorithm> sort(str.begin(), str.end()) 默认是从小到大排序, reverse() 反转字符串在[begin, end] 范围内的顺序.

--- PAT_basic_level/p_1009.cpp
+++ PAT_basic_level/p_1009.cpp
@@ -10,61 +10,48 @@
 
 using namespace std;
 
+string subtraction(string str1, string str2)
+{
+    // <string> stoi() 将字符串转为int
+    int result = stoi(str1) - stoi(str2);
+    // <string> to_string() 将数值 转为字符串
+    string tmp = to_string(result);
+    // 小于4位的整数, 补0
+    while (tmp.size() < 4)
+        tmp += "0";
+    
+    return tmp;
+}
+
 void p_1009()
 {
-    string str, str1, str2;
-    getline(cin, str);
-    str1 = str2 = str;
+    string str, max, min;
+    cin >> str;
+    // 小于4位的整数, 补0
+    while (str.size() < 4)
+        str += "0";
+    max = min = str;
     
-    char tmp;
+    // 利用sort() 函数(默认从小到大排序), 与.begin(), .end()相互配合
+    sort(min.begin(), min.end());
+    sort(max.begin(), max.end());
+    // reverse() 函数, 将字符串从后往前排序
+    reverse(max.begin(), max.end());
     
-    while (str != "6174")
+    if (max == min)
+        cout << max << " - " << min << " = 0000";
+    else
     {
-        for (int i = 3; i >= 0; i--)
+        while (subtraction(max, min) != "6174")
         {
-            for (int j = 0; j < i; j++)
-            {
-                if (str1[j] < str1[j+1])
-                {
-                    tmp = str1[j];
-                    str1[j] = str1[j+1];
-                    str1[j+1] = tmp;
-                }
-            }
-            cout << str1[3-i];
+            string tmp = subtraction(max, min);
+            cout << max << " - " << min << " = " << tmp << endl;
+            max = min = tmp;
+            sort(min.begin(), min.end());
+            sort(max.begin(), max.end());
+            reverse(max.begin(), max.end());
         }
-        
-        cout << " - ";
-        
-        for (int i = 3; i >= 0; i--)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (str2[j] > str2[j+1])
-                {
-                    tmp = str2[j];
-                    str2[j] = str2[j+1];
-                    str2[j+1] = tmp;
-                }
-            }
-            cout << str2[3-i];
-        }
-        
-        cout << " = ";
-        
-        for (int i = 3 ; i <= 0; i++)       // 减法运算
-        {
-            if (str1[i] >= str2[i])
-            {
-                str[i] = str1[i] - str2[i];
-            }
-            else
-            {
-                str[i] = str1[i] - str2[i] + 10;
-            }
-            cout << str[i];
-        }
-        cout << endl;
+        cout << max << " - " << min << " = " << subtraction(max, min);
     }
 }
 
